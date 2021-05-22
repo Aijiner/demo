d04153808ab3eff91300d7f7a31402e617b2b5ec
leihuazhuang <278620225@qq.com>
2021-05-22 13:59:56 +0800

    git commit test

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -9,10 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloWorld {
-
     @GetMapping("test")
     public String getTest(){
         System.err.println("test");
         return "Hello World";
     }
+
+    public static void main(String[] args) {
+        System.err.println(getMoney(3420,0.03,41));
+        System.err.println(getMoney(3420,0.12,41));
+    }
+
+    private static double getMoney(double money, double tax, int year) {
+        double reMoney = 0;
+        for (int i = 0; i < year; i++) {
+            reMoney = reMoney * (1 + tax) + money;
+//            System.err.println("money: " + reMoney);
+        }
+        return reMoney;
+    }
+
 }
